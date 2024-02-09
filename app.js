605b3614c50b0ae713067d0e17ef87d79df4d7b6
LuizaLorien <luiza.l.pereira@aluno.senai.br>
2024-02-09 07:32:48 -0300

    ja pode pular

--- app.js
+++ app.js
@@ -11,30 +11,31 @@ alt = Number(alt)
 let pes = peso.value //transformando as entradas em números
 pes = Number(pes)
 let calc = pes / (alt * alt) // calculo IMC
+calc = calc.toFixed(1)
 
 
 
 if(calc <= 18.4 && calc >= 17){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta abaixo do peso`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta abaixo do peso`
 }
 else if (calc < 16.9){
-    resultado.innerHTML =  `seu IMC é de: ${calc.toFixed(1)}, você esta severamente abaixo do peso`
+    resultado.innerHTML =  `seu IMC é de: ${calc}, você esta severamente abaixo do peso`
 }
 else if(calc <= 24.9 && calc >= 18.5){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta no peso ideal`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta no peso ideal`
 }
 else if(calc <= 29.9 && calc >= 25){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta acima do peso`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta acima do peso`
 }
 else if(calc <= 34.9 && calc >= 30){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta com obesidade tipo I`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta com obesidade tipo I`
 }
 else if(calc <= 40 && calc>= 35){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta com obesidade tipo II`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta com obesidade tipo II`
 }
 else if(calc > 40){
-    resultado.innerHTML = `seu IMC é de: ${calc.toFixed(1)}, você esta com obesidade tipo III`
+    resultado.innerHTML = `seu IMC é de: ${calc}, você esta com obesidade tipo III`
 }
-console.log(calc.toFixed(1))
+console.log(calc)
 }
 )
